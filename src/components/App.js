cc2d63420c5f727d94e7e68ce5575ded29acaa61
Dasun Sucharith <sucharith.dasun@gmail.com>
2022-06-02 18:07:36 +0530

    new

--- src/components/App.js
+++ src/components/App.js
@@ -2,21 +2,28 @@ import React, { Component } from 'react'
 import TrelloList from "./TrelloList";
 import { connect } from "react-redux";
 import TrelloActionButton from './TrelloActionButton';
+import { DragDropContext } from 'react-beautiful-dnd';
 
 class App extends Component {
+
+  onDragEnd = () => {
+    // TODO: reordering logic
+  }
 
   render() {
     const { lists } = this.props;
     return (
-      <div className="App">
-        <h2>Trello Clone</h2>
-        <div style={styles.listsContainer}>
-          {lists.map(list => (
-            <TrelloList listID={list.id} key={list.id} title={list.title} cards={list.cards} />
-          ))}
-          <TrelloActionButton list />
+      <DragDropContext onDragEnd={this.onDragEnd}>
+        <div className="App">
+          <h2>Trello Clone</h2>
+          <div style={styles.listsContainer}>
+            {lists.map(list => (
+              <TrelloList listID={list.id} key={list.id} title={list.title} cards={list.cards} />
+            ))}
+            <TrelloActionButton list />
+          </div>
         </div>
-      </div>
+      </DragDropContext>
     );
   }
 }
--- src/components/TrelloList.js
+++ src/components/TrelloList.js
@@ -1,17 +1,23 @@
 import React from 'react'
 import TrelloCard from './TrelloCard'
 import TrelloActionButton from './TrelloActionButton'
+import { Droppable } from 'react-beautiful-dnd'
 
 function TrelloList({title, cards, listID}) {
     return (
-    <div style={ styles.container }>
-            <h4 style={{ textAlign: 'center', }}>{title}</h4> 
-            {cards.map(card => (
-                <TrelloCard key={card.id} text={card.text} />
-            ))}   
-            <TrelloActionButton listID={listID} />
-    </div> 
-  )
+        <Droppable droppableId={String(listID)}>
+            {() => (
+                
+            ) }
+            <div style={ styles.container }>
+                <h4 style={{ textAlign: 'center', }}>{title}</h4> 
+                {cards.map(card => (
+                    <TrelloCard key={card.id} text={card.text} />
+                ))}   
+                <TrelloActionButton listID={listID} />
+            </div> 
+        </Droppable>
+    )
 }
 
 const styles = {
